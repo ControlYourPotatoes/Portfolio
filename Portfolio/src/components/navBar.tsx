5eadbd0aa5c7597415dd5c124dddf12bb7d35867
ControlYourPotatoes <alexanderpuga78@gmail.com>
2024-09-19 05:11:37 -0400

    chore: Update npm dependencies and add vite-plugin-svgr

--- Portfolio/src/components/navBar.tsx
+++ Portfolio/src/components/navBar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 
+import Logo from '../assets/logo.svg';
+
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray p-4 border-b border-orange">
       <div className="container mx-auto flex justify-end items-center">
+        <div className="w-16 h-16 self-center">
+          <img src={Logo} alt="Logo" className="w-16 h-16 mb-[-2rem]" />
+        </div>
         <ul className="flex space-x-6">
           {['About', 'Projects', 'Contact'].map((item) => (
             <li key={item}>
